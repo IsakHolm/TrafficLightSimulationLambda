3c7cb35a55731d1443814d76561f8b638b0d4494
IsakHolm <holmdahl.isak@gmail.com>
2023-02-10 13:48:39 +0100

    Inital commit, added files

---
+++ THESIS-bicycle-priority/handler.py
@@ -0,0 +1,22 @@
+import json
+import time
+
+
+def sim(event, context):
+    
+    now = time.time() % 30
+    
+    resDict = {
+        "status" : "green",
+        "time_left" : int( 15 - ( now % 15 ) )
+    }
+    
+    if now < 15:
+        resDict["status"] = "red"
+
+    #test
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(resDict)
+    }
