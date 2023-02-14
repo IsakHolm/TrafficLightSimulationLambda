641c95afb4494dc204633e16ed2914f58d0e5963
IsakHolm <holmdahl.isak@gmail.com>
2023-02-14 13:10:19 +0100

    Added position for two opposing traffic lights

--- THESIS-bicycle-priority/handler.py
+++ THESIS-bicycle-priority/handler.py
@@ -1,23 +1,29 @@
 import json
 import time
+from math import sin, cos, sqrt, atan2, radians
 
 
 def sim(event, context):
     
+    tl_1 = (57.70556493250048, 11.99211749254271)
+    tl_2 = (57.705567563648366, 11.992202034098431)
+
     now = time.time() % 30
     
-    resDict = {
+    res = {
         "status" : "green",
-        "time_left" : int( 15 - ( now % 15 ) )
+        "time_left" : int( 15 - ( now % 15 ) ),
+        "light_1_lat": tl_1[0],
+        "light_1_lon": tl_1[1],
+        "light_2_lat": tl_2[0],
+        "light_2_lon": tl_2[1]
     }
     
     if now < 15:
-        resDict["status"] = "red"
+        res["status"] = "red"
 
-    #test
-    #test2
     return {
         'statusCode': 200,
-        'body': json.dumps(resDict)
+        'body': json.dumps(res)
     }
 
