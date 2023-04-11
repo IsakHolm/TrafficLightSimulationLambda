b360a40e7445853b4b084561393ac79c92c00884
IsakHolm <holmdahl.isak@gmail.com>
2023-04-11 15:15:15 +0200

    Function now checks to see if cyclist is moving away from trafficlight
    Co-Authored-By: rstandar <98148916+rstandar@users.noreply.github.com>

--- THESIS-bicycle-priority/handler.py
+++ THESIS-bicycle-priority/handler.py
@@ -2,34 +2,35 @@ import json
 import time
 from math import sin, cos, sqrt, atan2, radians
 
+tl_1 = [57, 13]
+tl_2 = [57, 10]
+
+lights = [tl_1, tl_2]
 
 def sim(event, context):
-    
-    tl_1 = [57, 13]
-    tl_2 = [57, 10]
-
-    lights = [tl_1, tl_2]
 
     event_body = json.loads(event["body"])
     
     user_pos = [event_body["lat"], event_body["lon"]]
+    prev_user_pos = [event_body["prev_lat"], event_body["prev_lon"]]
 
-    min = calc_distance(float(user_pos[0]), float(user_pos[1]), lights[0][0], lights[0][1])
+    lightsList = sortTrafficLights(user_pos[0], user_pos[1])
+
     index = 0
 
-    for i, tl in enumerate(lights):
-        tmp = calc_distance(float(user_pos[0]), float(user_pos[1]), tl[0], tl[1])
-        if tmp < min: 
-            min = tmp
+    for i, tl in enumerate(lightsList):
+        if calc_distance(user_pos[0], user_pos[1], tl[0], tl[1]) < calc_distance(prev_user_pos[0], prev_user_pos[1], tl[0], tl[1]):
             index = i
+            break
+
 
     now = time.time() % 30
-    
+
     res = {
         "status" : "green",
         "time_left" : int( 15 - ( now % 15 ) ),
-        "light_lat": lights[index][0],
-        "light_lon": lights[index][1],
+        "light_lat": lightsList[index][0],
+        "light_lon": lightsList[index][1],
         "distance": min
     }
     
@@ -41,6 +42,25 @@ def sim(event, context):
         'body': json.dumps(res)
     }
 
+def searchTrafficLights(lat, lon, lightsCurrent):
+    min = calc_distance(float(lat), float(lon), lightsCurrent[0][0], lightsCurrent[0][1])
+    index = 0
+    for i, tl in enumerate(lightsCurrent):
+        tmp = calc_distance(float(lat), float(lon), tl[0], tl[1])
+        if tmp < min:
+            min = tmp
+            index = i
+
+    return (index, min)
+
+def sortTrafficLights(lat, lon):
+    resList = []
+    lightsCopy = lights.copy
+    for i in enumerate(lights):
+        res = searchTrafficLights(lat, lon, lightsCopy)
+        resList.append(lightsCopy.pop(res.index))
+
+    return resList
 
 def calc_distance(lat1, lon1, lat2, lon2):
     R = 6373.0
